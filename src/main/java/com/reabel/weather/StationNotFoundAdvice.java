1312d88e313d1333a2fb0bb430bf840e3eba5681
Ryan Reabel <ryanreabel@gmail.com>
2021-03-10 18:49:33 -0500

    cleanup old code, add better error handling

--- src/main/java/com/reabel/weather/StationNotFoundAdvice.java
+++ src/main/java/com/reabel/weather/StationNotFoundAdvice.java
@@ -6,13 +6,30 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.ModelAndView;
+
 @ControllerAdvice
 class StationNotFoundAdvice {
 
     @ResponseBody
     @ExceptionHandler(StationNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    String stationNotFoundHandler(StationNotFoundException ex) {
-        return ex.getMessage();
+    public ModelAndView stationNotFoundHandler(StationNotFoundException ex) {
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("message", ex.getMessage());
+        mv.setViewName("errors/station-not-found");
+
+        return mv;
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ModelAndView handle(Exception ex) {
+
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("message", ex.getMessage());
+        mv.setViewName("errors/404");
+
+        return mv;
     }
 }
